7b46575e2b4e2aeb0f9eb9235c51dd3d34823adf
lukaszmecala <lukaszfronia99@gmail.com>
2025-01-31 17:53:08 +0100

    configured tailwind css and added some simple styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
+import UserName from '../features/user/UserName'
 
 function Header() {
-  return (
-    <header>
-      <Link to="/">Fast Pizza React Co.</Link>
-      <SearchOrder />
-      <p>Lukasz</p>
-    </header>
-  );
+    return (
+        <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+            <Link to="/" className="tracking-widest">
+                Fast Pizza React Co.
+            </Link>
+            <SearchOrder />
+            <UserName />
+        </header>
+    )
 }
 
-export default Header;
+export default Header
